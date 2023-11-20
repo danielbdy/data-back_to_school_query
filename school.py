add6fcf932f68f0869ed4ce34d850d557cd6de54
daniel bowen <danielbdy@gmail.com>
2023-11-20 22:41:19 +0800

    fix as kitt shows Rake aborted

--- school.py
+++ school.py
@@ -1,7 +1,7 @@
 # pylint:disable=C0111,C0103
 
 def students_from_city(db, city):
-    """return a list of students from a specific city"""
+    # """return a list of students from a specific city"""
     query = '''
         SELECT * FROM students s
         WHERE birth_city = ?
